591bb121bf27613a4343df7c40812e3d18765e6b
Pritam Sharma <qurilotechnologiesllc@gmail.com>
2026-04-28 10:34:59 +0530

    add the frontend url in cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "*",
+    origin: "https://blog-cms-delta-nine.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"]
 }))
 
