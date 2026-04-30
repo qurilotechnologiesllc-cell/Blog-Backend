d01f43163fddb8a5ac30520d7f03eef7b77c7bef
Pritam Sharma <qurilotechnologiesllc@gmail.com>
2026-04-30 18:14:05 +0530

    now i am using brevo-sdk

--- src/utils/sendmail.js
+++ src/utils/sendmail.js
@@ -1,74 +1,56 @@
-const nodemailer = require("nodemailer");
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
-const transporter = nodemailer.createTransport({
+const defaultClient =
+    SibApiV3Sdk.ApiClient.instance;
 
-    host: "smtp-relay.brevo.com",
+const apiKey =
+    defaultClient.authentications["api-key"];
 
-    port: 587,
+apiKey.apiKey =
+    process.env.BREVO_API_KEY;
 
-    secure: false,
+const apiInstance =
+    new SibApiV3Sdk.TransactionalEmailsApi();
 
-    auth: {
-        user: process.env.BREVO_LOGIN,
-        pass: process.env.BREVO_PASS,
-    },
-
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000,
-});
-
-const sendOTPEmail = async (email, otp, role) => {
+const sendOTPEmail = async (
+    email,
+    otp
+) => {
 
     try {
 
-        await transporter.verify();
+        const response =
+            await apiInstance.sendTransacEmail({
 
-        const info = await transporter.sendMail({
+                sender: {
+                    email: process.env.BREVO_USER,
+                    name: "Qurilo Solution",
+                },
 
-            from: `"Blog App" <${process.env.BREVO_USER}>`,
+                to: [
+                    {
+                        email: email,
+                    },
+                ],
 
-            to: email,
+                templateId: 1,
 
-            subject:
-                role === "admin"
-                    ? "Admin Login OTP"
-                    : "Your OTP Verification",
+                params: {
+                    otp: otp,
+                },
+            });
 
-            html: `
-                <div style="font-family: Arial; padding: 20px;">
-
-                    <h2>
-                        ${role === "admin"
-                    ? "Admin Login Verification"
-                    : "Email Verification"
-                }
-                    </h2>
-
-                    <p>Your OTP is:</p>
-
-                    <h1 style="color: blue;">
-                        ${otp}
-                    </h1>
-
-                    <p>
-                        This OTP will expire in
-                        <strong>10 minutes</strong>.
-                    </p>
-
-                </div>
-            `
-        });
-
-        console.log("Email Sent:", info.messageId);
-
-        return info;
+        console.log(
+            "Email Sent:",
+            response
+        );
 
     } catch (error) {
 
-        console.log("Brevo Error:", error);
-
-        throw error;
+        console.log(
+            error.response?.body ||
+            error.message
+        );
     }
 };
 
